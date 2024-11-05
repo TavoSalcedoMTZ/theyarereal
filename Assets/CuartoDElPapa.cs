0a79a28f9247e4b1c7f3126ad349e344d337ad1e
TavoSalcedoMTZ <150973163+TavoSalcedoMTZ@users.noreply.github.com>
2024-11-05 14:35:44 -0600

    ramses me la croma
    arregle visual soy dios

--- Assets/CuartoDElPapa.cs
+++ Assets/CuartoDElPapa.cs
@@ -1,27 +1,24 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-
-public class CuartoDElPapa : MonoBehaviour
+public class CuartoDELPapa : MonoBehaviour
 {
     public TextMeshProUGUI dialogoText;
-
     void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            StartCoroutine(ShowAndHideText("No puedo entrar aquí, no sé cómo podría reaccionar"));
+            StartCoroutine(ShowAndHideText("No puedo entrar aquí"));
             Debug.Log("Espera");
         }
     }
-
     void OnTriggerExit(Collider collision)
     {
-        StopAllCoroutines(); // Detener cualquier coroutine activa
-        dialogoText.text = ""; // Limpiar el texto al salir
+        StopAllCoroutines(); 
+        dialogoText.text = ""; 
         Debug.Log("Espera");
     }
-
     private IEnumerator ShowAndHideText(string message)
     {
         // Mostrar el texto letra por letra
@@ -31,17 +28,15 @@ public class CuartoDElPapa : MonoBehaviour
             dialogoText.text += letter; // Añadir letra por letra
             yield return new WaitForSeconds(0.05f); // Esperar un poco antes de mostrar la siguiente letra
         }
-
         // Esperar un momento antes de empezar a borrar
         yield return new WaitForSeconds(1f);
-
         // Borrar el texto letra por letra
         for (int i = message.Length - 1; i >= 0; i--)
         {
+            Debug.Log("Error");
             dialogoText.text = dialogoText.text.Remove(i, 1); // Borrar letra por letra
             yield return new WaitForSeconds(0.05f); // Esperar un poco antes de borrar la siguiente letra
         }
-
         // Opcional: Reiniciar el proceso
         yield return new WaitForSeconds(1f);
         StartCoroutine(ShowAndHideText(message)); // Iniciar de nuevo
